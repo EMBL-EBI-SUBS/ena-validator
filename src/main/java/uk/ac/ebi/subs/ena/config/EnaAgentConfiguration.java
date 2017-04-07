39b08953fb57fdf09f6789c2a0fbe4328cc11866
Neil Goodgame <neilg@ebi.ac.uk>
2017-04-07 16:42:28 +0100

    Spring config and associated tests

---
+++ src/main/java/uk/ac/ebi/subs/ena/config/EnaAgentConfiguration.java
@@ -0,0 +1,101 @@
+package uk.ac.ebi.subs.ena.config;
+
+//import org.eclipse.persistence.jaxb.JAXBContextProperties;
+//import org.eclipse.persistence.jaxb.MarshallerProperties;
+import org.eclipse.persistence.jaxb.JAXBContextProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.Resource;
+import org.springframework.oxm.castor.CastorMarshaller;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import uk.ac.ebi.subs.data.submittable.ENAExperiment;
+import uk.ac.ebi.subs.data.submittable.ENARun;
+import uk.ac.ebi.subs.data.submittable.ENAStudy;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+
+
+@Configuration
+public class EnaAgentConfiguration {
+
+    String SUBMITTABLE_PACKAGE = "uk.ac.ebi.subs.data.submittable";
+    String COMPONENT_PACKAGE = "uk.ac.ebi.subs.data.component";
+
+    @Autowired
+    private ApplicationContext appContext;
+
+    @Value("classpath:uk/ac/ebi/subs/data/component/attribute_mapping.xml")
+    Resource componentMappingResource;
+
+    @Value("classpath:uk/ac/ebi/subs/data/submittable/study_mapping.xml")
+    Resource studyMappingResource;
+
+    @Value("classpath:uk/ac/ebi/subs/data/submittable/experiment_mapping.xml")
+    Resource experimentMappingResource;
+
+    @Value("classpath:uk/ac/ebi/subs/data/submittable/run_mapping.xml")
+    Resource runMappingResource;
+
+    @Bean(name = "study")
+    Jaxb2Marshaller jaxb2StudyMarshaller () throws IOException {
+        Class enaStudyClass = ENAStudy.class;
+        return getJaxb2Marshaller(studyMappingResource, enaStudyClass);
+    }
+
+    @Bean(name = "experiment")
+    Jaxb2Marshaller jaxb2ExperimentMarshaller () throws IOException {
+        Class enaExperimentClass = ENAExperiment.class;
+        return getJaxb2Marshaller(experimentMappingResource, enaExperimentClass);
+    }
+
+    @Bean(name = "run")
+    Jaxb2Marshaller jaxb2RunMarshaller () throws IOException {
+        Class enaRunClass = ENARun.class;
+        return getJaxb2Marshaller(runMappingResource, enaRunClass);
+    }
+
+    private Jaxb2Marshaller getJaxb2Marshaller(Resource resource, Class enaStudyClass) throws IOException {
+        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+        final HashMap<String, Object> jaxpHashMap = new HashMap<>();
+        jaxpHashMap.put(COMPONENT_PACKAGE, new StreamSource(componentMappingResource.getInputStream()));
+        jaxpHashMap.put(SUBMITTABLE_PACKAGE, new StreamSource(resource.getInputStream()));
+        Map<String, Object> properties = new HashMap<String, Object>();
+        properties.put(JAXBContextProperties.OXM_METADATA_SOURCE, jaxpHashMap);
+        jaxb2Marshaller.setJaxbContextProperties(properties);
+        jaxb2Marshaller.setClassesToBeBound(enaStudyClass);
+        return jaxb2Marshaller;
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.erareader_era")
+    DataSource dataSource() {
+        DataSource dataSource = DataSourceBuilder.create().build();
+        return dataSource;
+    }
+
+}
---
+++ src/test/java/uk/ac/ebi/subs/ena/config/EnaAgentConfigurationTest.java
@@ -0,0 +1,189 @@
+package uk.ac.ebi.subs.ena.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.oxm.Marshaller;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import uk.ac.ebi.ena.sra.xml.ID;
+import uk.ac.ebi.subs.data.component.AssayRef;
+import uk.ac.ebi.subs.data.component.Attribute;
+import uk.ac.ebi.subs.data.component.Team;
+import uk.ac.ebi.subs.data.submittable.*;
+import uk.ac.ebi.subs.ena.EnaAgentApplication;
+
+import javax.sql.DataSource;
+import javax.xml.bind.JAXBException;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.xpath.XPathFactory;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by neilg on 07/04/2017.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {EnaAgentApplication.class})
+public class EnaAgentConfigurationTest {
+
+    @Autowired
+    @Qualifier("study")
+    Marshaller studyMarshaller;
+
+    @Autowired
+    @Qualifier("experiment")
+    Marshaller experimentMarshaller;
+
+    @Autowired
+    @Qualifier("run")
+    Marshaller runMarshaller;
+
+    @Autowired
+    DataSource dataSource;
+
+    static final String team = "my-team";
+
+    DocumentBuilderFactory documentBuilderFactory = null;
+    XPathFactory xPathFactory = null;
+    ObjectMapper objectMapper = new ObjectMapper();
+    Marshaller marshaller;
+
+    @Before
+    public void setUp() throws IOException, JAXBException, URISyntaxException {
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        xPathFactory = XPathFactory.newInstance();
+    }
+
+    @Test
+    public void testJaxb2StudyMarshaller() throws Exception {
+        Study study = createStudy(UUID.randomUUID().toString(),team);
+        ENAStudy enaStudy = new ENAStudy(study);
+        final Document document = documentBuilderFactory.newDocumentBuilder().newDocument();
+        studyMarshaller.marshal(enaStudy,new DOMResult(document));
+        final Element documentElement = document.getDocumentElement();
+        assertNotNull(documentElement.getLocalName());
+    }
+
+    @Test
+    public void testJaxb2ExperimentMarshaller() throws Exception {
+        Assay assay = createAssay(UUID.randomUUID().toString(),team);
+        ENAExperiment enaExperiment = new ENAExperiment(assay);
+        final Document document = documentBuilderFactory.newDocumentBuilder().newDocument();
+        experimentMarshaller.marshal(enaExperiment,new DOMResult(document));
+        final Element documentElement = document.getDocumentElement();
+        assertNotNull(documentElement.getLocalName());
+    }
+
+    @Test
+    public void testJaxb2RunMarshaller() throws Exception {
+        AssayData assayData = createAssayData(UUID.randomUUID().toString(),team);
+        ENARun enaRun = new ENARun(assayData);
+        final Document document = documentBuilderFactory.newDocumentBuilder().newDocument();
+        runMarshaller.marshal(enaRun,new DOMResult(document));
+        final Element documentElement = document.getDocumentElement();
+        assertNotNull(documentElement.getLocalName());
+    }
+
+    @Test
+    public void testDataSource() throws Exception {
+        Connection connection = dataSource.getConnection();
+        final boolean valid = connection.isValid(100);
+        connection.close();
+        assertTrue(valid);
+    }
+
+
+    public Study createStudy (String name, String team) {
+        Study study = new Study();
+        study.setAlias(name);
+        study.setTeam(createTeam(team));
+        study.setDescription("Description");
+        study.setTitle("Title");
+        Attribute attribute = new Attribute();
+        attribute.setName(ENAStudy.EXISTING_STUDY_TYPE);
+        attribute.setValue(UUID.randomUUID().toString());
+        study.getAttributes().add(attribute);
+        return study;
+    }
+
+    public Assay createAssay (String name, String team) {
+        Assay assay = new Assay();
+        assay.setAlias(name);
+        assay.setTeam(createTeam(team));
+        assay.setDescription("Description");
+        assay.setTitle("Title");
+        Attribute libraryNameAttribute = new Attribute();
+        libraryNameAttribute.setName(ENAExperiment.LIBRARY_NAME);
+        libraryNameAttribute.setValue(ENAExperiment.LIBRARY_NAME);
+        assay.getAttributes().add(libraryNameAttribute);
+        Attribute designDescriptionAttribute = new Attribute();
+        designDescriptionAttribute.setName(ENAExperiment.DESIGN_DESCRIPTION);
+        designDescriptionAttribute.setValue(ENAExperiment.DESIGN_DESCRIPTION);
+        assay.getAttributes().add(designDescriptionAttribute);
+        Attribute libraryLayoutAttribute = new Attribute();
+        libraryLayoutAttribute.setName(ENAExperiment.LIBRARY_LAYOUT);
+        libraryLayoutAttribute.setValue(ENAExperiment.SINGLE);
+        assay.getAttributes().add(libraryLayoutAttribute);
+        Attribute libraryStratagy = new Attribute();
+        libraryStratagy.setName(ENAExperiment.LIBRARY_STRATEGY);
+        libraryStratagy.setValue("WGS");
+        assay.getAttributes().add(libraryStratagy);
+        Attribute librarySource = new Attribute();
+        librarySource.setName(ENAExperiment.LIBRARY_SOURCE);
+        librarySource.setValue("Genomic");
+        Attribute librarySelection = new Attribute();
+        librarySelection.setName(ENAExperiment.LIBRARY_SELECTION);
+        librarySelection.setValue("Random");
+        assay.getAttributes().add(librarySelection);
+        Attribute platformAtribute = new Attribute();
+        platformAtribute.setName(ENAExperiment.PLATFORM_TYPE);
+        platformAtribute.setValue("ILLUMINA");
+        assay.getAttributes().add(platformAtribute);
+        Attribute instrumentModelAttribute = new Attribute();
+        instrumentModelAttribute.setName(ENAExperiment.INSTRUMENT_MODEL);
+        instrumentModelAttribute.setValue("Illumina Genome Analyzer");
+        assay.getAttributes().add(instrumentModelAttribute);
+        return assay;
+    }
+
+    public AssayData createAssayData (String name, String team) {
+        AssayData assayData = new AssayData();
+        assayData.setAlias(name);
+        assayData.setTeam(createTeam(team));
+        assayData.setDescription("Description");
+        assayData.setTitle("Title");
+        uk.ac.ebi.subs.data.component.File file = new uk.ac.ebi.subs.data.component.File();
+        file.setName("test.fastq");
+        file.setChecksumMethod("MD5");
+        file.setChecksum("12345678123456781234567812345678");
+        file.setType("fastq");
+        assayData.getFiles().add(file);
+        AssayRef assayRef = new AssayRef();
+        assayRef.setAccession(UUID.randomUUID().toString());
+        assayData.setAssayRef(assayRef);
+        return assayData;
+    }
+
+    public Team createTeam (String teamString) {
+        Team team = new Team();
+        team.setName(teamString);
+        return team;
+    }
+
+
+}
