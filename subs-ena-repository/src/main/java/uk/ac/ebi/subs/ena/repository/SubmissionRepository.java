93f9d1e3382c311c05057fa56999af22d9d2a4cd
Neil Goodgame <neilg@ebi.ac.uk>
2017-04-25 17:09:11 +0100

    Refactored

--- subs-ena-repository/src/main/java/uk/ac/ebi/subs/ena/repository/SubmissionRepository.java
+++ subs-ena-repository/src/main/java/uk/ac/ebi/subs/ena/repository/SubmissionRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
  * Created by neilg on 02/04/2017.
  */
 public interface SubmissionRepository extends CrudRepository<Submission,String> {
-    List<Submission> findBySubmissionAccountId(String submissionAccountId);
+    //List<Submission> findBySubmissionAccountId(String submissionAccountId);
 }
