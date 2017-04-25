3969bc98497514cae224b2e4ab6fc0d3fbb29168
Neil Goodgame <neilg@ebi.ac.uk>
2017-04-25 11:14:23 +0100

    Test case for XMLType

---
+++ subs-ena-repository/src/test/java/uk/ac/ebi/subs/ena/type/XMLTypeTest.java
@@ -0,0 +1,49 @@
+package uk.ac.ebi.subs.ena.type;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by neilg on 25/04/2017.
+ */
+public class XMLTypeTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void nullSafeGet() throws Exception {
+    }
+
+    @Test
+    public void nullSafeSet() throws Exception {
+    }
+
+    @Test
+    public void deepCopy() throws Exception {
+    }
+
+    @Test
+    public void isMutable() throws Exception {
+    }
+
+    @Test
+    public void disassemble() throws Exception {
+    }
+
+    @Test
+    public void assemble() throws Exception {
+    }
+
+    @Test
+    public void replace() throws Exception {
+    }
+
+}
