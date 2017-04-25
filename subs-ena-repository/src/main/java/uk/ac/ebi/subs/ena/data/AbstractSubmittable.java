a27de6e500ed9cec060bea7a2c79a7902e501e8a
Neil Goodgame <neilg@ebi.ac.uk>
2017-04-25 17:10:25 +0100

    Added firstCreated, egaID, and version

--- subs-ena-repository/src/main/java/uk/ac/ebi/subs/ena/data/AbstractSubmittable.java
+++ subs-ena-repository/src/main/java/uk/ac/ebi/subs/ena/data/AbstractSubmittable.java
@@ -2,18 +2,33 @@ package uk.ac.ebi.subs.ena.data;
 
 import org.w3c.dom.Document;
 
-import javax.persistence.Column;
+import javax.persistence.*;
+import java.sql.Timestamp;
 
 /**
  * Created by neilg on 01/04/2017.
  */
-public abstract class AbstractSubmittable<T extends AbstractSubmittable> implements Submittable{
+@MappedSuperclass
+public abstract class AbstractSubmittable<T extends AbstractSubmittable> implements Submittable {
     @Column(name = "SUBMISSION_ID")
-    protected String submissionId = null;
-    @Column(name="STATUS_ID")
+    String submissionId = null;
+    @Column(name = "STATUS_ID")
     int statusId;
-    @Column(name="SUBMISSION_ACCOUNT_ID")
+
+    @Column(name = "SUBMISSION_ACCOUNT_ID")
     String submissionAccountId;
+
+    @Column(name = "VERSION")
+    String version;
+
+    @Column(name = "EGA_ID")
+    String egaId;
+
+    @Column(name = "FIRST_CREATED")
+    Timestamp firstCreated;
+
+    public AbstractSubmittable() {
+    }
 
     public AbstractSubmittable(String submissionId, int statusId) {
         this.submissionId = submissionId;
@@ -44,5 +59,37 @@ public abstract class AbstractSubmittable<T extends AbstractSubmittable> impleme
     @Override
     public void setSubmissionAccountId(String submissionAccountId) {
         this.submissionAccountId = submissionAccountId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getEgaId() {
+        return egaId;
+    }
+
+    public void setEgaId(String egaId) {
+        this.egaId = egaId;
+    }
+
+    public Timestamp getFirstCreated() {
+        return firstCreated;
+    }
+
+    public void setFirstCreated(Timestamp firstCreated) {
+        this.firstCreated = firstCreated;
+    }
+
+    public SubmissionStatus getSubmissionStatus() {
+        return SubmissionStatus.getSubmissionStatus(statusId);
+    }
+
+    public void setSubmissionStatus(SubmissionStatus submissionStatus) {
+        setStatusId(submissionStatus.getStatusId());
     }
 }
