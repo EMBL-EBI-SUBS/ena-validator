d0d77c01eac1f1f2bdde90ebe4ad87208bd4dbfc
Neil Goodgame <neilg@ebi.ac.uk>
2017-04-26 07:50:43 +0100

    Experiment

---
+++ subs-ena-repository/src/main/java/uk/ac/ebi/subs/ena/data/Experiment.java
@@ -0,0 +1,50 @@
+package uk.ac.ebi.subs.ena.data;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.w3c.dom.Document;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="EXPERIMENT")
+public class Experiment extends AbstractSubmittable<Experiment> {
+
+    @Id
+    @GenericGenerator(name = "sraExperimentGen", strategy = "uk.ac.ebi.subs.ena.id.ExperimentIDGenerator")
+    @GeneratedValue(generator = "sraExperimentGen")
+    @Column(name = "EXPERIMENT_ID")
+    String id;
+
+    @Column(name = "EXPERIMENT_ALIAS")
+    String alias;
+
+    @Column(name="EXPERIMENT_XML")
+    @Type(type="uk.ac.ebi.subs.ena.type.XMLType")
+    Document document;
+
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    @Override
+    public void setId(String id) {
+
+    }
+
+    @Override
+    public String getAlias() {
+        return null;
+    }
+
+    @Override
+    public Document getDocument() {
+        return null;
+    }
+
+    @Override
+    public void setDocument(Document document) {
+
+    }
+}
