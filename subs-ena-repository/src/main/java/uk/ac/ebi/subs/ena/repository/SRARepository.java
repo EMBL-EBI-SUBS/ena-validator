dd8d527f753f9bd09c4699bcdbc529bc9820a901
Neil Goodgame <neilg@ebi.ac.uk>
2017-04-27 09:25:46 +0100

    Used by submittable objects

--- subs-ena-repository/src/main/java/uk/ac/ebi/subs/ena/repository/SRARepository.java
+++ subs-ena-repository/src/main/java/uk/ac/ebi/subs/ena/repository/SRARepository.java
@@ -1,9 +1,9 @@
 package uk.ac.ebi.subs.ena.repository;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import uk.ac.ebi.subs.ena.data.AbstractSubmittable;
+import uk.ac.ebi.subs.ena.data.AbstractSRAInfo;
+import uk.ac.ebi.subs.ena.data.SRAInfo;
 
 import java.util.List;
 
@@ -11,8 +11,8 @@ import java.util.List;
  * Created by neilg on 25/04/2017.
  */
 @NoRepositoryBean
-public interface SubmittableRepository<T extends AbstractSubmittable<T>> extends PagingAndSortingRepository<T, String> {
-    List<T> findBySubmissionId(String submissionId);
+public interface SRARepository<T extends SRAInfo> extends PagingAndSortingRepository<T, String> {
+    //List<T> findBySubmissionId(String submissionId);
     T findByAliasAndSubmissionAccountId(String alias, String submissionAccountId);
 
 }
