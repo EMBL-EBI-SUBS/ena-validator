1f7bf25205691bd876f3e42708f3e913ffab42be
Neil Goodgame <neilg@ebi.ac.uk>
2017-04-27 09:22:38 +0100

    Base object for all submittables ie db objects that have a submission id

--- subs-ena-repository/src/main/java/uk/ac/ebi/subs/ena/data/Submittable.java
+++ subs-ena-repository/src/main/java/uk/ac/ebi/subs/ena/data/Submittable.java
@@ -1,32 +1,16 @@
 package uk.ac.ebi.subs.ena.data;
 
-import org.w3c.dom.Document;
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
 
 /**
- * Created by neilg on 02/04/2017.
+ * Created by neilg on 26/04/2017.
  */
 public interface Submittable {
-    public String getId();
-
-    public void setId(String id);
-
-    public String getAlias();
-
-    public Document getDocument();
-
-    public void setDocument(Document document);
-
     public String getSubmissionId();
-
     public void setSubmissionId(String submissionId);
-
     public int getStatusId();
-
     public void setStatusId(int statusId);
-
-    public String getSubmissionAccountId();
-
-    public void setSubmissionAccountId(String submissionAccountId);
-
-
+    public SubmissionStatus getSubmissionStatus();
+    public void setSubmissionStatus(SubmissionStatus submissionStatus);
 }
