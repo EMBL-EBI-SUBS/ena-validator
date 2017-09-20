6de48d3e5c6e3a535b95ede87cc2f1554a47668c
Flavia Penim <flaviapenim@gmail.com>
2017-09-20 14:07:05 +0100

    Update validator-common version and fix broken code.

--- subs-ena-validator/src/main/java/uk/ac/ebi/subs/ena/validator/ENAExperimentValidator.java
+++ subs-ena-validator/src/main/java/uk/ac/ebi/subs/ena/validator/ENAExperimentValidator.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
