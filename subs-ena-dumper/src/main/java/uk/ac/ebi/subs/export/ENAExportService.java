b67aa82f32812dd90a1f40f2fd35edba0e6dcb89
Neil Goodgame <neilg@ebi.ac.uk>
2017-09-20 17:01:27 +0100

    Added the option to dump by submission

--- subs-ena-dumper/src/main/java/uk/ac/ebi/subs/export/ENAExportService.java
+++ subs-ena-dumper/src/main/java/uk/ac/ebi/subs/export/ENAExportService.java
@@ -5,40 +5,73 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import uk.ac.ebi.subs.ena.data.Submission;
+import uk.ac.ebi.subs.ena.data.SubmittableSRAInfo;
+import uk.ac.ebi.subs.ena.repository.SubmissionRepository;
 
 import javax.xml.bind.JAXBException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@EnableConfigurationProperties(ENAExportServiceConfiguration.class)
+@EnableConfigurationProperties(ExportServiceProperties.class)
 public class ENAExportService {
 
+    private static final int PAGE_SIZE = 10000;
     List<ExportService> exportServiceList = new ArrayList<>();
     final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    ENAExportServiceConfiguration enaExportServiceConfiguration;
-
+    ExportServiceProperties exportServiceProperties;
+    SubmissionRepository submissionRepository;
 
     public ENAExportService(SampleExportService sampleExportService,
                             StudyExportService studyExportService,
                             ExperimentExportService experimentExportService,
-                            ENAExportServiceConfiguration enaExportServiceConfiguration) {
+                            SubmissionRepository submissionRepository,
+                            ExportServiceProperties exportServiceProperties) {
         exportServiceList.add(sampleExportService);
         exportServiceList.add(studyExportService);
         exportServiceList.add(experimentExportService);
-        this.enaExportServiceConfiguration = enaExportServiceConfiguration;
+        this.exportServiceProperties = exportServiceProperties;
+        this.submissionRepository = submissionRepository;
     }
 
     public void export () {
-        List<String> exportServiceNameList =  Arrays.asList(enaExportServiceConfiguration.getServices());
-        String submissionAccountId = enaExportServiceConfiguration.getSubmissionAccountId();
-        final Path exportDirPath = Paths.get(enaExportServiceConfiguration.getPath(), submissionAccountId);
+        List<String> exportServiceNameList =  Arrays.asList(exportServiceProperties.getServices());
+        String submissionAccountId = exportServiceProperties.getSubmissionAccountId();
+        final Path exportDirPath = Paths.get(exportServiceProperties.getPath(), submissionAccountId);
+
+        if (exportServiceProperties.isBySubmission()) {
+            Pageable page = new PageRequest(0, PAGE_SIZE);
+            long rowCount = submissionRepository.countBySubmissionAccountId(submissionAccountId);
+
+            for (int i = 0; i * PAGE_SIZE < rowCount; i++) {
+                final List<Submission> submissionList = submissionRepository.findBySubmissionAccountId(submissionAccountId, page);
+
+                for (Submission submission : submissionList) {
+                    for (ExportService exportService : exportServiceList) {
+                        if (exportServiceNameList.contains(exportService.getName())) {
+                            final Path pathForDate = AbstractExportService.getPathForDate(exportDirPath.resolve("SUBMISSION"), submission);
+                            exportService.exportBySubmissionId(pathForDate, submission.getId());
+                        }
+
+                    }
+                }
+                logger.info("dumped submissions " + page.getPageSize() + " records in page " + page.getPageNumber());
+                page = page.next();
+            }
+
+            submissionRepository.countBySubmissionAccountId(submissionAccountId);
+
+        }
 
         for (ExportService exportService : exportServiceList) {
             if (exportServiceNameList.contains(exportService.getName())) {
