0c01c2ef3320862b54b3ec6a2fe35411ae24058b
Neil Goodgame <neilg@ebi.ac.uk>
2017-09-20 10:08:48 +0100

    Overrided getSubmittable to set the release date to the current date

---
+++ subs-ena-dumper/src/main/java/uk/ac/ebi/subs/export/StudyExportService.java
@@ -0,0 +1,31 @@
+package uk.ac.ebi.subs.export;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.env.SystemEnvironmentPropertySource;
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Node;
+import uk.ac.ebi.subs.data.submittable.ENAStudy;
+import uk.ac.ebi.subs.data.submittable.MappingHelper;
+import uk.ac.ebi.subs.data.submittable.Study;
+import uk.ac.ebi.subs.ena.data.SubmittableSRAInfo;
+import uk.ac.ebi.subs.ena.repository.StudyRepository;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.xpath.XPathExpressionException;
+
+@Service
+public class StudyExportService extends AbstractExportService<Study,ENAStudy> {
+
+    public static final String STUDY_XPATH_EXPRESSION = "/STUDY_SET/STUDY[1]";
+
+    public StudyExportService(StudyRepository studyRepository, ObjectMapper objectMapper) {
+        super(studyRepository, ENAStudy.class, MappingHelper.STUDY_MARSHALLER, STUDY_XPATH_EXPRESSION,objectMapper);
+    }
+
+    @Override
+    protected Study getSubmittable(SubmittableSRAInfo submittable) throws XPathExpressionException, JAXBException, IllegalAccessException {
+        final Study study = super.getSubmittable(submittable);
+        study.setReleaseDate(new java.util.Date());
+        return study;
+    }
+}
