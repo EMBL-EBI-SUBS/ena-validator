114270de4ea42a283444edfca52885d15e142015
Neil Goodgame <neilg@ebi.ac.uk>
2017-09-22 17:02:53 +0100

    Cleaned up calls enaExportService

--- subs-ena-dumper/src/main/java/uk/ac/ebi/subs/ExportApp.java
+++ subs-ena-dumper/src/main/java/uk/ac/ebi/subs/ExportApp.java
@@ -2,7 +2,6 @@ package uk.ac.ebi.subs;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
@@ -15,15 +14,14 @@ import org.springframework.boot.context.TypeExcludeFilter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import uk.ac.ebi.subs.export.ENAExportService;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import uk.ac.ebi.subs.export.ENASubmittableExportService;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration(exclude={MongoRepositoriesAutoConfiguration.class,MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @ComponentScan(excludeFilters = {
         @ComponentScan.Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
         @ComponentScan.Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class) })
+
 public class ExportApp implements CommandLineRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(ExportApp.class);
