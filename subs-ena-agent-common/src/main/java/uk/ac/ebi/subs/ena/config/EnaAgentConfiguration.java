7153ae4a23c4ec07301b0bcd8d86d0aed8c22738
Neil Goodgame <neilg@ebi.ac.uk>
2017-11-10 16:29:22 +0000

    Removed dataSource bean

--- subs-ena-agent-common/src/main/java/uk/ac/ebi/subs/ena/config/EnaAgentConfiguration.java
+++ subs-ena-agent-common/src/main/java/uk/ac/ebi/subs/ena/config/EnaAgentConfiguration.java
@@ -1,30 +1,18 @@
 package uk.ac.ebi.subs.ena.config;
 
-//import org.eclipse.persistence.jaxb.JAXBContextProperties;
-//import org.eclipse.persistence.jaxb.MarshallerProperties;
-
 import org.eclipse.persistence.jaxb.JAXBContextProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.Resource;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import uk.ac.ebi.ena.authentication.model.AuthResult;
-import uk.ac.ebi.ena.sra.SRALoader;
 import uk.ac.ebi.subs.data.submittable.ENAExperiment;
 import uk.ac.ebi.subs.data.submittable.ENARun;
 import uk.ac.ebi.subs.data.submittable.ENASample;
 import uk.ac.ebi.subs.data.submittable.ENAStudy;
 
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
 import java.util.HashMap;
@@ -56,25 +44,25 @@ public class EnaAgentConfiguration {
     Resource sampleMappingResource;
 
     @Bean(name = "study")
-    Jaxb2Marshaller jaxb2StudyMarshaller () throws IOException {
+    Jaxb2Marshaller jaxb2StudyMarshaller() throws IOException {
         Class enaStudyClass = ENAStudy.class;
         return getJaxb2Marshaller(studyMappingResource, enaStudyClass);
     }
 
     @Bean(name = "sample")
-    Jaxb2Marshaller jaxb2SampleMarshaller () throws IOException {
+    Jaxb2Marshaller jaxb2SampleMarshaller() throws IOException {
         Class enaSampleClass = ENASample.class;
         return getJaxb2Marshaller(sampleMappingResource, enaSampleClass);
     }
 
     @Bean(name = "experiment")
-    Jaxb2Marshaller jaxb2ExperimentMarshaller () throws IOException {
+    Jaxb2Marshaller jaxb2ExperimentMarshaller() throws IOException {
         Class enaExperimentClass = ENAExperiment.class;
         return getJaxb2Marshaller(experimentMappingResource, enaExperimentClass);
     }
 
     @Bean(name = "run")
-    Jaxb2Marshaller jaxb2RunMarshaller () throws IOException {
+    Jaxb2Marshaller jaxb2RunMarshaller() throws IOException {
         Class enaRunClass = ENARun.class;
         return getJaxb2Marshaller(runMappingResource, enaRunClass);
     }
@@ -89,40 +77,6 @@ public class EnaAgentConfiguration {
         jaxb2Marshaller.setJaxbContextProperties(properties);
         jaxb2Marshaller.setClassesToBeBound(enaStudyClass);
         return jaxb2Marshaller;
-    }
-
-    @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource.erareader_era")
-    DataSource dataSource() {
-        DataSource dataSource = DataSourceBuilder.create().build();
-        return dataSource;
-    }
-
-    @Bean
-    public JpaTransactionManager transactionManager()
-    {
-        return new JpaTransactionManager();
-    }
-
-
-    @Bean
-    SRALoader sraLoader(@Value("${ena.transaction_mode}") String transactionMode) {
-        SRALoader sraLoader = new SRALoader();
-        sraLoader.setTransactionMode(SRALoader.TransactionMode.valueOf(transactionMode));
-        return sraLoader;
-    }
-
-    @Bean
-    AuthResult authResult(@Value("${ena.principal}") String principal, @Value("${ena.login_name}")  String loginName) {
-        AuthResult authResult = new AuthResult();
-        authResult.setLoginName(loginName);
-        authResult.setPrinciple(principal);
-        Map<String, Boolean> roles = new HashMap<>();
-        roles.put("EGA", false);
-        roles.put("SUPER_USER", false);
-        authResult.setRoles(roles);
-        return authResult;
     }
 
 }
