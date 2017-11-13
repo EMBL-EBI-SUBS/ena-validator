da79d0f194ba98bb282529cde1a690d2a4ce4dc1
Neil Goodgame <neilg@ebi.ac.uk>
2017-11-13 16:19:03 +0000

    Added test that checks that a duplicate submission fails

--- subs-ena-agent-common/src/test/java/uk/ac/ebi/subs/ena/processor/ENAStudyProcessorTest.java
+++ subs-ena-agent-common/src/test/java/uk/ac/ebi/subs/ena/processor/ENAStudyProcessorTest.java
@@ -56,10 +56,47 @@ public class ENAStudyProcessorTest {
         final Study study = TestHelper.getStudy(alias, team,"study_abstract", "Whole Genome Sequencing");
         final LocalDate date = LocalDate.parse("2018-12-25");
         study.setReleaseDate(date);
+        final ProcessingStatusEnum processingStatusEnum = ProcessingStatusEnum.Received;
+        processSubmission(study, processingStatusEnum);
+    }
+
+    @Test
+    public void processDuplicateSubmission() throws Exception {
+        String alias = UUID.randomUUID().toString();
+        final Team team = TestHelper.getTeam("test-team");
+        final Study study = TestHelper.getStudy(alias, team,"study_abstract", "Whole Genome Sequencing");
+        final LocalDate date = LocalDate.parse("2018-12-25");
+        study.setReleaseDate(date);
+        final ProcessingStatusEnum processingStatusEnum = ProcessingStatusEnum.Received;
+        processSubmission(study, processingStatusEnum);
+        final ProcessingStatusEnum processingStatusEnum2 = ProcessingStatusEnum.Error;
+        final Study study2 = TestHelper.getStudy(alias, team,"study_abstract", "Whole Genome Sequencing");
+        final LocalDate date2 = LocalDate.parse("2018-12-25");
+        study2.setReleaseDate(date2);
+        study2.setId(UUID.randomUUID().toString());
+        final ArrayList<SingleValidationResult> singleValidationResultList = new ArrayList<>();
+        final ProcessingCertificate processingCertificate = enaStudyProcessor.processAndConvertSubmittable(study2, singleValidationResultList);
+        assertThat(processingCertificate, is(equalTo(new ProcessingCertificate(study2, Archive.Ena, processingStatusEnum2, null))));
+    }
+
+    private void processSubmission(Study study, ProcessingStatusEnum processingStatusEnum) {
         study.setId(UUID.randomUUID().toString());
         final ArrayList<SingleValidationResult> singleValidationResultList = new ArrayList<>();
         final ProcessingCertificate processingCertificate = enaStudyProcessor.processAndConvertSubmittable(study, singleValidationResultList);
-        assertThat(processingCertificate, is(equalTo(new ProcessingCertificate(study, Archive.Ena, ProcessingStatusEnum.Received, study.getAccession()))));
+        assertThat(processingCertificate, is(equalTo(new ProcessingCertificate(study, Archive.Ena, processingStatusEnum, study.getAccession()))));
+    }
+
+    @Test
+    public void processInvalidStudyTypeSubmission() throws Exception {
+        String alias = UUID.randomUUID().toString();
+        final Team team = TestHelper.getTeam("test-team");
+        final Study study = TestHelper.getStudy(alias, team,"study_abstract", "Blah");
+        final LocalDate date = LocalDate.parse("2018-12-25");
+        study.setReleaseDate(date);
+        study.setId(UUID.randomUUID().toString());
+        final ArrayList<SingleValidationResult> singleValidationResultList = new ArrayList<>();
+        final ProcessingCertificate processingCertificate = enaStudyProcessor.processAndConvertSubmittable(study, singleValidationResultList);
+        assertThat(processingCertificate, is(equalTo(new ProcessingCertificate(study, Archive.Ena, ProcessingStatusEnum.Error, null))));
     }
 
 }
