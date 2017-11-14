f9332d8a7692741fdd8c0b855d351153df2355bc
Neil Goodgame <neilg@ebi.ac.uk>
2017-11-14 10:53:51 +0000

    Removed alias

--- subs-ena-agent-common/src/main/java/uk/ac/ebi/subs/ena/loader/SRALoaderService.java
+++ subs-ena-agent-common/src/main/java/uk/ac/ebi/subs/ena/loader/SRALoaderService.java
@@ -7,7 +7,7 @@ import uk.ac.ebi.subs.data.submittable.ENASubmittable;
  * Created by neilg on 12/04/2017.
  */
 public interface SRALoaderService<T extends ENASubmittable> {
-    boolean executeSRASubmission(T enaSubmittable, String submissionAlias, boolean validateOnly) throws Exception;
+    boolean executeSRASubmission(T enaSubmittable, boolean validateOnly) throws Exception;
     boolean executeSRASubmission(String submissionXML, String submittableXML) throws Exception;
     //ValidationResult getValidationResult();
     String getSchema ();
