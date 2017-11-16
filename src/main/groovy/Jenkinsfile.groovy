8100654e75dacc009b4c4dcd3f51eb48542acfac
Neil Goodgame <neilg@ebi.ac.uk>
2017-11-16 16:24:50 +0000

    Added jenkins build file

---
+++ src/main/groovy/Jenkinsfile.groovy
@@ -0,0 +1,23 @@
+node {
+    stage('Preparation') { // for display purposes
+        // Get some code from a GitHub repository
+        checkout scm
+        //gradleHome = tool 'gradle-3'
+    }
+    stage('Build') {
+        // Run the gradle assemble
+        echo 'Building'
+        sh "./gradlew assemble"
+    }
+    stage('Deploy') {
+        sh 'echo branch $BRANCH_NAME'
+        sh 'git name-rev --name-only HEAD > GIT_BRANCH'
+        sh 'cat GIT_BRANCH'
+        git_branch = readFile('GIT_BRANCH').trim()
+        if (git_branch == 'remotes/origin/dev') {
+            sh "./gradlew --gradle-user-home=/homes/sub_adm/secrets -PsshKeyFile=/var/lib/jenkins/.ssh/id_rsa -Penv=dev deployJar"
+        } else if (git_branch == 'remotes/origin/master') {
+            sh "./gradlew --gradle-user-home=/homes/sub_adm/secrets -PsshKeyFile=/var/lib/jenkins/.ssh/id_rsa -Penv=test deployJar"
+        }
+    }
+}
