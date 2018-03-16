70cb7072583be34aa83213809d4327821cb8a97f
Neil Goodgame <neilg@ebi.ac.uk>
2018-03-16 14:24:11 +0000

    validateStudy method listens to rabbitmq and constructs a SubmissionEnvelope to represent the sample being validated.  The supporting samples that represents sample relationships submitted in the same submission are also included.

--- src/main/java/uk/ac/ebi/subs/ena/validator/ENASampleValidator.java
+++ src/main/java/uk/ac/ebi/subs/ena/validator/ENASampleValidator.java
@@ -1,18 +1,21 @@
 package uk.ac.ebi.subs.ena.validator;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uk.ac.ebi.subs.data.Submission;
 import uk.ac.ebi.subs.data.submittable.Sample;
-import uk.ac.ebi.subs.ena.processor.ENAProcessorContainerService;
-import uk.ac.ebi.subs.ena.processor.ENASampleProcessor;
+import uk.ac.ebi.subs.data.submittable.Study;
+import uk.ac.ebi.subs.ena.processor.ENAProcessor;
+import uk.ac.ebi.subs.processing.SubmissionEnvelope;
 import uk.ac.ebi.subs.validator.data.SampleValidationMessageEnvelope;
 import uk.ac.ebi.subs.validator.data.SingleValidationResult;
+import uk.ac.ebi.subs.validator.data.StudyValidationMessageEnvelope;
+import uk.ac.ebi.subs.validator.data.ValidationMessageEnvelope;
+import uk.ac.ebi.subs.validator.model.Submittable;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static uk.ac.ebi.subs.ena.config.EnaValidatorQueues.ENA_SAMPLE_VALIDATION;
 
@@ -20,54 +23,35 @@ import static uk.ac.ebi.subs.ena.config.EnaValidatorQueues.ENA_SAMPLE_VALIDATION
  * This class responsible to do the ENA related validations.
  */
 @Service
-public class ENASampleValidator implements ENAValidator {
+public class ENASampleValidator extends ENAValidator<Sample> {
 
-    private static final Logger logger = LoggerFactory.getLogger(ENASampleValidator.class);
-
-    @Autowired
-    ENASampleProcessor enaSampleProcessor;
-
-    public ENASampleProcessor getEnaSampleProcessor() {
-        return enaSampleProcessor;
-    }
-
-    public void setEnaSampleProcessor(ENASampleProcessor eNASampleProcessor) {
-        this.enaSampleProcessor = eNASampleProcessor;
-    }
-
-    RabbitMessagingTemplate rabbitMessagingTemplate;
-
-    ENAProcessorContainerService enaProcessorContainerService;
-
-    @Override
-    public void setRabbitMessagingTemplate(RabbitMessagingTemplate rabbitMessagingTemplate) {
-        this.rabbitMessagingTemplate = rabbitMessagingTemplate;
-    }
-
-    @Override
-    public RabbitMessagingTemplate getRabbitMessagingTemplate() {
-        return rabbitMessagingTemplate;
-    }
-
-    public ENASampleValidator(RabbitMessagingTemplate rabbitMessagingTemplate,
-                              ENAProcessorContainerService enaProcessorContainerService) {
-        this.rabbitMessagingTemplate = rabbitMessagingTemplate;
-        this.enaProcessorContainerService = enaProcessorContainerService;
+    public ENASampleValidator(ENAProcessor enaProcessor, RabbitMessagingTemplate rabbitMessagingTemplate) {
+        super(enaProcessor, rabbitMessagingTemplate);
     }
 
     /**
-     * Do a validation for the sample submitted in the {@link SampleValidationMessageEnvelope}.
+     * Do a validation for the sample submitted in the {@link StudyValidationMessageEnvelope}.
      * It produces a message according to the validation outcome.
      *
-     * @param validationEnvelope {@link SampleValidationMessageEnvelope} that contains the sample to validate
+     * @param validationEnvelope {@link StudyValidationMessageEnvelope} that contains the sample to validate
      * @throws InstantiationException
      * @throws IllegalAccessException
      */
     @RabbitListener(queues = ENA_SAMPLE_VALIDATION)
     public void validateSample(SampleValidationMessageEnvelope validationEnvelope) {
         final Sample sample = validationEnvelope.getEntityToValidate();
-        final List<SingleValidationResult> singleValidationResultCollection = executeSubmittableValidation(sample, enaSampleProcessor);
-        checkForEmptySingleValidationResult(singleValidationResultCollection,sample);
-        publishValidationMessage(sample,singleValidationResultCollection,validationEnvelope.getValidationResultUUID(),validationEnvelope.getValidationResultVersion());
+        SubmissionEnvelope submissionEnvelope = new SubmissionEnvelope();
+        submissionEnvelope.getSamples().add(sample);
+        final List<Sample> supportingSampleList = validationEnvelope.getSampleList().stream().
+                filter(s -> validationEnvelope.getSubmissionId().equals(s.getSubmissionId())).
+                map(s -> s.getBaseSubmittable()).
+                collect(Collectors.toList());
+        submissionEnvelope.getSamples().addAll(supportingSampleList);
+        final List<SingleValidationResult> singleValidationResultList = validate(submissionEnvelope,sample);
+        publishValidationMessage(validationEnvelope.getEntityToValidate(),
+                singleValidationResultList,
+                validationEnvelope.getValidationResultUUID(),
+                validationEnvelope.getValidationResultVersion());
     }
+
 }
